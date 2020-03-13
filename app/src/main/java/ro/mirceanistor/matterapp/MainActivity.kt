e35d77fa01f7e93f8bc8ef0c068fc31271cd2683
Rahul <64.rahulraj@gmail.com>
2020-03-13 11:46:50 +0530

    Update gradle and migrate to androidx

--- app/src/main/java/ro/mirceanistor/matterapp/MainActivity.kt
+++ app/src/main/java/ro/mirceanistor/matterapp/MainActivity.kt
@@ -1,6 +1,6 @@
 package ro.mirceanistor.matterapp
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 import ro.mirceanistor.matterlib.MaterialState
